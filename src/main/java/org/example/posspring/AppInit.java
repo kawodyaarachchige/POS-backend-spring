d2df85b4f95b11edc8b481721dfcf282070bd3ef
Tharushi Kawodya Arachchige <139765472+kawodyaarachchige@users.noreply.github.com>
2024-10-09 13:22:18 +0530

    created packages

---
+++ src/main/java/org/example/posspring/AppInit.java
@@ -0,0 +1,23 @@
+package org.example.posspring;
+
+import org.example.posspring.config.WebAppConfig;
+import org.example.posspring.config.WebAppRootConfig;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class AppInit extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{WebAppRootConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{WebAppConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+
+}
---
+++ src/main/java/org/example/posspring/config/WebAppRootConfig.java
@@ -0,0 +1,50 @@
+package org.example.posspring.config;
+
+import jakarta.persistence.EntityManagerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableJpaRepositories(basePackages = "org.example.posspring.dao")
+@EnableTransactionManagement
+@ComponentScan(basePackages = "org.example.posspring")
+public class WebAppRootConfig {
+    @Bean
+    public DataSource dataSource(){
+        var dmds = new DriverManagerDataSource();
+        dmds.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dmds.setUrl("jdbc:mysql://localhost:3306/springPos?createDatabaseIfNotExist=true");
+        dmds.setUsername("root");
+        dmds.setPassword("Ijse@1234");
+        return dmds;
+    }
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+
+        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        vendorAdapter.setGenerateDdl(true);
+
+        LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+        factory.setJpaVendorAdapter(vendorAdapter);
+        factory.setPackagesToScan("org.example.posspring.entity.impl");
+        factory.setDataSource(dataSource());
+        return factory;
+    }
+    @Bean
+    PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+
+        JpaTransactionManager txManager = new JpaTransactionManager();
+        txManager.setEntityManagerFactory(entityManagerFactory);
+        return txManager;
+    }
+}
